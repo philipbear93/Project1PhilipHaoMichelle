56abfa9e863a608934e42924823b077ec95f42e6
Hao Li <li.138@wright.edu>
2016-10-16 20:28:29 -0400

    update the commnets and plotting

--- belshfuncsrod.m
+++ belshfuncsrod.m
@@ -1,8 +1,8 @@
-% Derive Shape functions for quadratic beam element
+% Derive Shape functions for rod/torsion element
 % For all points, the polynomials N and N' are evaluated.
-% w=a+bx+cx^2+dx^3+ex^4+fx^5
+% w=a+bx
 b=[1 -1; % w(-1) x^n
-   1  1];
+   1  1];% w(1) x^n
 b=fliplr(b);
 disp('b flipped to decreasing power order')
 % For N1
@@ -13,5 +13,8 @@ c1d=polyderiv(c1)
 a=[0 1]';
 c2=(b\a)'
 c2d=polyderiv(c2)
+
+i=-1:.01:1;
+plot(i,polyval(c1,i),i,polyval(c2,i))
 
 %plot(i,polyval(c1,i),"-;sf1;",i,polyval(c2,i),"-;sf2;",i,polyval(c3,i),"-;sf3;",i,polyval(c4,i),"-;sf4;",i,polyval(c5,i),"-;sf5;",i,polyval(c6,i),"-;sf6;")
